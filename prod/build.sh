d86b827bc1f1780a95bf3c8e07347162a6549672
Salmaan Saeed <salsaeed999@gmail.com>
2023-07-13 00:47:19 -0400

    the production script removes debug symbols to decrease the size of the binary

--- prod/build.sh
+++ prod/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # Debug, ReleaseSafe, ReleaseSmall, ReleaseFast are the options for -O
-zig build-exe ../src/main.zig -lc -lSDL2 -lSDL2_image -lSDL2_mixer -O ReleaseSafe -flto
+zig build-exe ../src/main.zig -lc -lSDL2 -lSDL2_image -lSDL2_mixer -O ReleaseSafe -flto -fstrip
